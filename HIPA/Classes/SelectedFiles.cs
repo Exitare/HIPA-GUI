4ab5e59c9531b66a9295062167c28e9be8fd9013
Exitare <business.exitare@icloud.com>
2019-01-15 12:21:30 -0800

    Add first Steps

---
+++ HIPA/Classes/SelectedFiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIPA
+{
+    class SelectedFiles
+    {
+
+        private int _ID;
+        private string _Name;
+
+        public SelectedFiles(int ID, string Name)
+        {
+            _ID = ID;
+            _Name = Name;
+        }
+
+        public string Name { get => _Name; set => _Name = value; }
+        public int ID { get => _ID; set => _ID = value; }
+    }
+}
---
+++ HIPA/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using HIPA;
+
+namespace HIPA
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void testDragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effects = DragDropEffects.All;
+            }
+        }
+
+        private void testDragEnter(object sender, DragEventArgs e)
+        {
+            calculationBox.Text = "Enter";
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                calculationBox.Text = files[0];
+            }
+        }
+
+        private void testDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Copy;
+            else
+                e.Effects = DragDropEffects.None;
+        }
+
+        private void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            calculationBox.Text = "Entered";
+            Console.WriteLine("DragEnter");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            // Set the file dialog to filter for graphics files.
+            openFileDialog.Filter = "Text|*.txt|All|*.*";
+
+            // Allow the user to select multiple images.
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Select your TimeFrame Files";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (String file in openFileDialog.FileNames)
+                {
+                    calculationBox.AppendText(openFileDialog.FileName + "\n");
+                    HIPA.Globals.Files.Add(new SelectedFiles(0, openFileDialog.FileName));
+                }
+            }
+               
+        }
+    }
+}
